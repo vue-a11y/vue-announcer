c06569cbf819a63a37a618b42afd3abff9fd19a3
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-05-20 12:07:01 -0300

    Add Vue component vue-a11y-announcer

---
+++ src/vue-a11y-announcer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    id="announcer"
+    class="announcer"
+    aria-live="polite"
+    aria-atomic="true"
+    v-text="content">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueA11YAnnouncer',
+  data () {
+    return {
+      content: ''
+    }
+  },
+  created () {
+    this.$announcer.data = this.$data
+  }
+}
+</script>
+
+<style scoped>
+.announcer {
+  position: absolute;
+  left: -10000px;
+  top: auto;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+}
+</style>
