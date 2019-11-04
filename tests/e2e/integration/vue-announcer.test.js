f264c32c65674de862701fdfd0c632f1651eca91
Luke Dawkes <luke.dawkes@yourdolphin.com>
2019-11-04 12:03:44 +0000

    Added $announcer.setComplementRoute
    + Added TypeScript definitions
    + Added Cypress tests
    ~ Modified example routes to use a beforeEnter handler so that tests can be run against the new method

--- tests/e2e/integration/vue-announcer.test.js
+++ tests/e2e/integration/vue-announcer.test.js
@@ -12,18 +12,31 @@ describe('Announcer test', () => {
     cy.get('[data-va="announcer"]').should('contain', 'has loaded')
   })
 
+  it('Should contain the custom complement when the route changes', () => {
+    cy.get('a[href="/contact"]').click()
+    cy.get('[data-va="announcer"]').should('contain', 'has fully loaded')
+  })
+
+  it('Should handle setting the custom complement multiple times', () => {
+    cy.get('a[href="/contact"]').click()
+    cy.get('[data-va="announcer"]').should('contain', 'has fully loaded')
+
+    cy.get('a[href="/about"]').click()
+    cy.get('[data-va="announcer"]').should('contain', 'has loaded')
+  })
+
   it('Should be equal toasted message with the announced', () => {
     cy.get('a[href="/about"]').click()
     cy.get('[data-va="toasted"]').click()
 
     cy.get('.toasted-container')
-      .find('.toasted')
-      .invoke('text')
-      .then(text1 => {
-        cy.get('[data-va="announcer"]').invoke('text').should(text2 => {
-          console.log(text1, text2)
-          expect(text1).to.eq(text2)
+        .find('.toasted')
+        .invoke('text')
+        .then(text1 => {
+          cy.get('[data-va="announcer"]').invoke('text').should(text2 => {
+            console.log(text1, text2)
+            expect(text1).to.eq(text2)
+          })
         })
-      })
   })
 })
