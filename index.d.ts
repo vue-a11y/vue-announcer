a8134bc48a579bb133ba09a1795a64f9634983c6
Alan Ktquez <ktquez@gmail.com>
2020-03-01 14:32:51 -0300

    Update ts support

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,14 @@
-import {PluginFunction} from 'vue';
+import { PluginFunction } from 'vue';
 
 export interface Announcer
 {
     data: Record<string, any>;
 
+    options: Record<string, object>;
+
     set(message: string): void;
+
+    reset(): void;
 
     setComplementRoute(complementRoute: string): void;
 }
